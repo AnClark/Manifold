722bf1d3352029d79f78a01c717e3917bd40cf8e
AnClark <clarklaw4701@qq.com>
2026-04-22 19:13:12 +0800

    Introduce miniaudio library

---
+++ deps/miniaudio/miniaudio_impl.cpp
@@ -0,0 +1,5 @@
+// miniaudio single-header implementation unit.
+// MINIAUDIO_IMPLEMENTATION must be defined in exactly ONE translation unit.
+// All other files that need miniaudio types just #include "miniaudio.h" without this define.
+#define MINIAUDIO_IMPLEMENTATION
+#include "miniaudio.h"
