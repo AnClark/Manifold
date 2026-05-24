b0e2a3ebf1f85dae014cfbb41a8ffe466eff21b3
AnClark <clarklaw4701@qq.com>
2026-05-24 13:39:59 +0800

    refactor(AnalyzerDispatcher): simplify requestCancelProcessing method by using `for` iteration

--- src/dispatchers/AnalyzerDispatcher.cpp
+++ src/dispatchers/AnalyzerDispatcher.cpp
@@ -52,10 +52,10 @@ void AnalyzerDispatcher::addFiles(SndFileList& fileListFromApp)
 
 void AnalyzerDispatcher::requestCancelProcessing()
 {
-    for (auto iter = ebuR128WorkerPool_.begin(); iter != ebuR128WorkerPool_.end(); iter++)
-        iter->get()->requestCancelProcessing();
-    for (auto iter = dcOffsetWorkerPool_.begin(); iter != dcOffsetWorkerPool_.end(); iter++)
-        iter->get()->requestCancelProcessing();
+    for (auto& w : ebuR128WorkerPool_)
+        w->requestCancelProcessing();
+    for (auto& w : dcOffsetWorkerPool_)
+        w->requestCancelProcessing();
 }
 
 void AnalyzerDispatcher::resumeProcessing()
